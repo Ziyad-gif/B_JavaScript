d119ef553e97d1c15908d531bd2f80357482876e
ziyad <ziyadjamalali@gmail.com>
2021-09-01 23:29:53 +0700

    done

--- ziyad/Program.cs
+++ ziyad/Program.cs
@@ -9,6 +9,7 @@ namespace ziyad
             Console.WriteLine("Hello World!");
             System.Console.WriteLine("ziyad");
             System.Console.WriteLine("kamil");
+            System.Console.WriteLine(2+5);
             Console.ReadLine();
             
         }
