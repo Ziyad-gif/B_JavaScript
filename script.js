5b31ada29425fc676e25d1a6b67fb226d90e3b79
ziyad <ziyadjamalali@gmail.com>
2021-09-01 23:04:42 +0700

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+var angka = prompt("masukan angka");
+
+if (angka % 2 === 0) {
+    alert("genap");
+} else {
+    alert("ganjil");
+}
---
+++ ziyad.cs
@@ -0,0 +1,11 @@
+using system;
+
+namespace MyNamespace
+{
+    class ziyad{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World");
+    }
+}
+}
